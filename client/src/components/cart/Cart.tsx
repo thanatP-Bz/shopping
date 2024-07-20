09a0e5746f4c789920f42c77c1fa7258b8e45903
Thanat <bztzn512@gmail.com>
2024-07-19 21:26:56 -0400

    cartAdded

--- client/src/components/cart/Cart.tsx
+++ client/src/components/cart/Cart.tsx
@@ -12,6 +12,7 @@ interface Item {
   id: number;
   img: Img;
   title: string;
+  desc: string;
   isNew: boolean;
   oldPrice: number;
   price: number;
@@ -21,7 +22,8 @@ const data: Item[] = [
   {
     id: 1,
     img: { img1 },
-    title: "shirt",
+    title: "long sleeve shirt",
+    desc: "The Everyday Shirt is the button-down that puts in work eight days a week.",
     isNew: true,
     oldPrice: 19,
     price: 12,
@@ -30,6 +32,7 @@ const data: Item[] = [
     id: 2,
     img: { img2 },
     title: "shirt",
+    desc: "The Everyday Shirt is the button-down that puts in work eight days a week.",
     isNew: true,
     oldPrice: 19,
     price: 12,
@@ -45,15 +48,18 @@ const Cart = () => {
           <img src={item.img.img1 ?? item.img.img2 ?? ""} alt="" />
           <div className="details">
             <h1>{item.title}</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui,
-              reprehenderit.
-            </p>
+            <p>{item.desc.substring(0, 100)}</p>
             <div className="price">1 x {item.price}</div>
           </div>
+          <DeleteOutlinedIcon className="delete" />
         </div>
       ))}
-      <DeleteOutlinedIcon className="delete" />
+      <div className="total">
+        <span>SUPTOTAL</span>
+        <span>$123</span>
+      </div>
+      <button>PROCESS TO CHECKOUT</button>
+      <span className="reset">Reset Cart</span>
     </div>
   );
 };
